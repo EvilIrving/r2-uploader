efa15e7d00b389b59e63f723f9ac999f765c78b0
Garcia <dong.yy1916@gmail.com>
2024-10-07 01:46:57 +0800

    fix: R2URL

--- src/store/d1.js
+++ src/store/d1.js
@@ -3,8 +3,11 @@ import { defineStore } from "pinia";
 
 export const useCloudflareConfigStore = defineStore("cloudflareConfig", {
   state: () => ({
-    // 移除敏感信息
+    R2_PARH: "https://pub-9350f14105fb48d49bb0de3e2822bc9e.r2.dev/",
   }),
+  getters: {
+    R2_URL: (state) => (name) => `${state.R2_PARH}${name}`,
+  },
   actions: {
     async queryDatabase(sql, params) {
       try {
